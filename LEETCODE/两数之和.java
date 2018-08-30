57d67d8cc349f9fafb7f61de80fd957920ef9bfa
hishark <hishark@yeah.net>
2018-08-30 10:20:38 +0800

    2018.8.30

---
+++ LEETCODE/两数之和.java
@@ -0,0 +1,37 @@
+class MySolution {
+    public int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        int result[] = new int[2];
+        loop:
+        for(int i=0;i<length;i++) {
+            for(int j=0;j<length;j++) {
+                if(nums[i]+nums[j]==target&&j!=i) {
+                    result[0] = i;
+                    result[1] = j;
+                    break loop;
+                }
+            }
+            
+        }
+        return result;
+    }
+}
+
+class OthersSolution{
+    public int[] twoSum(int[] nums,int target) {
+        int result[] = new int[2];
+        if(nums==null||nums.length<2)
+            return result;
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++) {
+            if(!map.containsKey(target - nums[i])) {
+                map.put(nums[i], i);
+            }else {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
