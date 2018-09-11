badaa0a2602c2a1398afcda43c296f2a6b81807f
hishark <hishark@yeah.net>
2018-09-11 23:19:22 +0800

    2018.9.11

--- OTHER/[DP]-最大连续子序列和.cpp
+++ OTHER/[DP]-最大连续子序列和.cpp
@@ -1,12 +1,38 @@
 /**
 给定一个数字序列A1,A2,....,An，求i,j使得Ai+...+Aj最大，输出这个最大和 
 input
+6
 -2 11 -4 13 -5 -2
 output 
+20
 **/
 #include<iostream>
-#include<vector>
+#include<algorithm>
 using namespace std;
+const int maxn = 10010; 
+int A[maxn],dp[maxn];//A[i]存放序列，dp[i]存放以A[i]结尾的连续序列的最大和 
 int main(){
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){//读入序列 
+		cin>>A[i]; 
+	} 
 	
+	//边界
+	dp[0] = A[0];
+	
+	for(int i=1;i<n;i++){
+		//状态转移方程
+		dp[i] = max(A[i],dp[i-1]+A[i]); 
+	} 
+	 
+	//dp[i]存放以A[i]结尾的连续序列的最大和，需要遍历i得到最大的才是结果
+	int max = 0;
+	for(int i=1;i<n;i++){
+		if(dp[i]>dp[max]){
+			max = i;
+		}
+	}
+	cout<<dp[max]<<endl;
+	return 0;
 } 
---
+++ PAT/PATA1007-[DP]最大子序列和.cpp
@@ -0,0 +1,11 @@
+/**
+Sample Input:
+10
+-10 1 2 3 4 -5 -23 3 7 -21
+Sample Output:输出从左到右最先遇到的方案 
+10 1 4
+
+**/
+#include<iostream>
+#include<algorithm>
+
