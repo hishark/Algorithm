592ed802e1159f63e27e492d9207a0b2f831e203
777 <hishark@yeah.net>
2019-10-02 19:12:41 +0800

    Update 27. 移除元素.java

--- LEETCODE/27. 移除元素.java
+++ LEETCODE/27. 移除元素.java
@@ -10,3 +10,19 @@ class Solution {
         return i;
     }   
 }
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        int n = nums.length;
+        while(i<n){
+            if(nums[i]==val){
+                nums[i] = nums[n-1];
+                n--;
+            }else{
+                i++;
+            }
+        }
+        return n;
+    }   
+}
