f3855f57f6cfb9ddd51eb184d95bf61ddd242729
777 <hishark@yeah.net>
2019-10-28 00:00:28 +0800

    Update 121. 买卖股票的最佳时机.java

--- LEETCODE/121. 买卖股票的最佳时机.java
+++ LEETCODE/121. 买卖股票的最佳时机.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for(int price: prices){
+            if(price<minprice){
+                minprice = price;
+            }
+            if(price-minprice>maxprofit){
+                maxprofit = price - minprice;
+            }
+        }
+        return maxprofit;
+    }
+}
