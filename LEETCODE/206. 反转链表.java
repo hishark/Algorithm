27d15f06a5fe085043185197dafd31ef836ea5fc
777 <hishark@yeah.net>
2019-11-15 09:39:24 +0800

    Update 206. 反转链表.java

--- LEETCODE/206. 反转链表.java
+++ LEETCODE/206. 反转链表.java
@@ -7,15 +7,26 @@
  * }
  画个图贼清晰
  */
+//递归
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) 
+        return head;
+    ListNode reversed = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return reversed;
+}
+
+//迭代
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null||head.next==null){
-            return head;
+        ListNode pre = null, cur = head;
+        while(cur!=null){
+            ListNode curNextTemp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = curNextTemp;
         }
-        ListNode next = head.next;
-        ListNode newHead = reverseList(next);
-        next.next = head;
-        head.next = null;
-        return newHead;
+        return pre;
     }
 }
