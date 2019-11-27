3d1f1e8fbcadf94026b6d8bb6bc5425d30ad2c82
777 <hishark@yeah.net>
2019-11-27 20:46:16 +0800

    Update 110. 平衡二叉树.java

--- LEETCODE/110. 平衡二叉树.java
+++ LEETCODE/110. 平衡二叉树.java
@@ -10,7 +10,7 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
         int height = tree_height(root);
-        return height >= 0;
+        return height >= 0;//只要height不为-1即为平衡二叉树 所以这里可以直接return height != -1
     }
     
     public int tree_height(TreeNode root){
@@ -18,6 +18,7 @@ class Solution {
             return 0;
         int left_height = tree_height(root.left);
         int right_height = tree_height(root.right);
+        //左右子树的高度如果相差超过1，那么表示不是平衡二叉树，直接返回-1即可 
         if(left_height==-1 || right_height==-1 || left_height-right_height>1 || right_height-left_height>1)
             return -1;
         return (left_height > right_height ? left_height : right_height) + 1;//别忘记+1啊！！
