b90413fb5a979d784a69b955880a099ed92ad2e3
777 <hishark@yeah.net>
2019-11-30 16:46:34 +0800

    Update 122. 买卖股票的最佳时机 II.java

--- LEETCODE/122. 买卖股票的最佳时机 II.java
+++ LEETCODE/122. 买卖股票的最佳时机 II.java
@@ -2,26 +2,32 @@
 //明天再来贪心 看电影去886
 class Solution {
     public int maxProfit(int[] prices) {
-        //获得从0开始的最大利润
+        //获得从第1天到最后一天的最大利润
         return getMaxProfit(prices, 0);
     }
     
     public int getMaxProfit(int[] prices, int start){
+        //越界返回0
         if(start>=prices.length){
             return 0;
         }
+        //初始化最大利润为0
         int maxProfit = 0;
-        for(int s = start;s<prices.length;s++){
-            int curMaxProfit = 0;
+       
+        //遍历0到prices.length-1
+        for(int s = start;s<prices.length;s++){            
+            //遍历s+1到prices.length-1
             for(int i=s+1;i<prices.length;i++){
+                //仅当当日股票价格比买入股票价格贵时，才叠加利润
                 if(prices[s] < prices[i]){
+                    //第i天的最大利润 = 第i+1天的最大利润 + 第i天的利润
+                    //第i天的利润 = 第i天的股票价格 - 股票的买入价格
                     int profit = getMaxProfit(prices, i+1) + prices[i] - prices[s];
-                    if(profit > curMaxProfit){
-                        curMaxProfit = profit;
+                    
+                    //更新最大利润
+                    if(profit > maxProfit){
+                        maxProfit = profit;
                     }
-                }
-                if(curMaxProfit > maxProfit){
-                    maxProfit = curMaxProfit;
                 }
             }
         }
