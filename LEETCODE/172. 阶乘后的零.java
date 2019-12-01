af70c67432592c2a0a67e8845f2522865ea4f22c
777 <hishark@yeah.net>
2019-12-01 20:51:48 +0800

    Create 172. 阶乘后的零.java

---
+++ LEETCODE/172. 阶乘后的零.java
@@ -0,0 +1,12 @@
+//龟龟 我是想不到哦-..-
+//https://leetcode-cn.com/problems/factorial-trailing-zeroes/solution/xiang-xi-tong-su-de-si-lu-fen-xi-by-windliang-3/
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n / 5;
+            n = n / 5;
+        }
+        return count;
+    }
+}
