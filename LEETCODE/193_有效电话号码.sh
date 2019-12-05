57f9ff7c039d05ac55ffca57413abc8f1592260c
777 <hishark@yeah.net>
2019-12-05 19:01:59 +0800

    Create 193_有效电话号码.sh

---
+++ LEETCODE/193_有效电话号码.sh
@@ -0,0 +1,7 @@
+grep "^([0-9]\{3\})\s[0-9]\{3\}-[0-9]\{4\}\b\|^[0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}$" file.txt
+
+正则表达式 - 语法 https://www.runoob.com/regexp/regexp-syntax.html
+
+Linux grep 命令 https://www.runoob.com/linux/linux-comm-grep.html
+
+Linux三大利器grep，sed，awk https://segmentfault.com/a/1190000015885994?utm_source=tag-newest
