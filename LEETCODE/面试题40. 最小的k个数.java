579eb6ea8275026809acf07ce596beafed7f7da9
hishark <hishark@yeah.net>
2020-03-20 23:43:51 +0800

    update

---
+++ LEETCODE/面试题40. 最小的k个数.java
@@ -0,0 +1,12 @@
+// 谁面试敢直接sort会当场被刷把哈哈哈哈哈哈
+// class Solution(object):
+//     def getLeastNumbers(self, arr, k):
+//         """
+//         :type arr: List[int]
+//         :type k: int
+//         :rtype: List[int]
+//         """
+//         arr.sort()
+//         return arr[:k]
+
+
