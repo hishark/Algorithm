bc2e4553ba49c90cd0383c1a8bd5a9cb47ca9bb7
hishark <hishark@yeah.net>
2020-05-21 23:58:22 +0800

    update

---
+++ LEETCODE/152. 乘积最大子数组.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        for(int i=0;i<nums.length;i++) {
+            int res = nums[i];
+            // 数组长度为1的情况，别漏了
+            if(res>max){
+                max = res;
+            }
+            for(int j=i+1;j<nums.length;j++) {
+                res = res * nums[j];
+                if(res>max){
+                    max = res;
+                }
+            }
+        }
+        return max;
+    }                               
+}
