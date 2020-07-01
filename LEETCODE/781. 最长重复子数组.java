6afd88ee7504dcdf0eb37480c9777c42904e1611
777 <hishark@yeah.net>
2020-07-01 12:12:38 +0800

    update

--- LEETCODE/781. 最长重复子数组.java
+++ LEETCODE/781. 最长重复子数组.java
@@ -19,3 +19,26 @@ class Solution {
 }
 
 
+// 动态规划
+class Solution {
+    public int findLength(int[] A, int[] B) {
+        // 状态数组
+        // dp[i][j]表示A[i:]和B[j:]最长公共前缀的长度
+        int[][] dp = new int[A.length+1][B.length+1];
+        
+        // 结果
+        int ans = 0;
+        
+        for (int i=A.length-1;i>=0;i--) {
+            for(int j=B.length-1;j>=0;j--) {
+                // 状态转移方程
+                // dp[i][j] = dp[i+1][j+1] + 1 (A[i]==B[j])
+                // dp[i][j] = 0 (A[i]!=B[j])
+                dp[i][j] = A[i]==B[j] ? dp[i+1][j+1] + 1 : 0;
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        
+        return ans;
+    }
+}
