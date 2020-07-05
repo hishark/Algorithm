f77afc39c64ca3274b6241e61dfa4943f7d02afb
777 <hishark@yeah.net>
2020-07-05 14:24:06 +0800

    update

--- LEETCODE/32. 最长有效括号.java
+++ LEETCODE/32. 最长有效括号.java
@@ -1,3 +1,4 @@
+// 栈解法
 class Solution {
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
@@ -33,6 +34,40 @@ class Solution {
             }
         }
 
+        return maxLen;
+    }
+}
+
+// 动态规划解法
+class Solution {
+    public int longestValidParentheses(String str) {
+        // 状态数组
+        // 初始值全都是0
+        int[] dp = new int[str.length()];
+        
+        char[] s = str.toCharArray();
+        
+        int maxLen = 0;
+        
+        // 所有'('的dp值直接为0
+        // ')'的dp值需要进一步计算
+        // 从i=1开始遍历，因为i=0一个人无法凑出一对
+        for (int i=1;i<s.length;i++) {
+            // 如果s[i]=')'且s[i-1]='(' 那么配对成功
+            if (s[i] == ')' && s[i-1] == '(') {
+                
+                dp[i] = ( i >=2 ? dp[i-2] : 0 ) + 2;
+            } else if (s[i] == ')' && s[i-1] == ')') {// 如果s[i]和s[i-1]都是')' 画个图缕清一下关系就成了
+                // 如果s[i-dp[i-1]-1]='(' 那么和s[i]配对成功
+                if ( i - dp[i-1] >= 1 && s[i - dp[i-1] - 1] == '(') {
+                    // i-dp[i-1]>=2 是为了保证 i-dp[i-1]-2不越界
+                    dp[i] = dp[i - 1] + ( i - dp[i-1] >= 2 ? dp[i - dp[i-1] -2] : 0) + 2;
+                }
+                
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        
         return maxLen;
     }
 }
