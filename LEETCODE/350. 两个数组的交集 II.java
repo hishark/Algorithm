0ac295af7fdf6383fee5d08689c7ec234f231e68
777 <hishark@yeah.net>
2020-07-14 10:12:45 +0800

    update

--- LEETCODE/350. 两个数组的交集 II.java
+++ LEETCODE/350. 两个数组的交集 II.java
@@ -42,3 +42,34 @@ class Solution {
         return Arrays.copyOfRange(res, 0, index);
     }
 }
+
+
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // 双指针
+        int i=0, j=0;
+
+        // 将数组排好序
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int[] res = new int[nums1.length < nums2.length ? nums1.length : nums2.length];
+        int index = 0;
+
+        // 指针未越界就一直遍历
+        while (i<nums1.length && j<nums2.length) {
+            if(nums1[i] == nums2[j]) {
+                res[index++] = nums1[i];
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]){
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return Arrays.copyOfRange(res, 0, index);
+    }
+}
