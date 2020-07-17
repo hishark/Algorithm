50b224923b71acb2d8837fe0c4f016934aad5a38
777 <hishark@yeah.net>
2020-07-17 15:12:16 +0800

    Update 35. 搜索插入位置.java

--- LEETCODE/35. 搜索插入位置.java
+++ LEETCODE/35. 搜索插入位置.java
@@ -1,42 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
-        int right = nums.length-1;
-        int temp=0;
-        while(left<right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target){
+        int right = nums.length - 1;
+        int mid = left + (right - left)/2;
+        while (left <= right) {
+            if(nums[mid] == target){
                 return mid;
-            }else if(nums[mid]<target){
-                left=mid+1;
-            }else{
-                right=mid-1;
-            }
-        }
-        if(target>nums[left])
-            return left+1;
-        else
-            return left;
-    }
-}
+            } else if (nums[mid] < target) {
+                left++;
+                mid = left + (right - left)/2;
+            } else {
+                right--;
+                mid = left + (right - left)/2;
+            }         
+        }   
 
-最后四行其实直接把left<right改成left<=right然后最后return left就好了！⬇️
-
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-        int temp=0;
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]<target){
-                left=mid+1;
-            }else{
-                right=mid-1;
-            }
-        }
         return left;
     }
 }
