458c8c3f22cfb756f1497cd19a213cb60156f5c6
777 <hishark@yeah.net>
2020-07-27 23:11:12 +0800

    Create 392. 判断子序列.java

---
+++ LEETCODE/392. 判断子序列.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+               i++; 
+            }
+            j++;
+        }
+        
+        return i == s.length();
+    }
+}
