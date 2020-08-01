886d1fdfd1001229469b20c1b732086eed6c83d9
777 <hishark@yeah.net>
2020-08-02 00:49:10 +0800

    Create 剑指 Offer 27. 二叉树的镜像.java

---
+++ LEETCODE/剑指 Offer 27. 二叉树的镜像.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null)
+            return null;
+        
+        TreeNode tmp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(tmp);
+        
+        return root;
+    }
+}
