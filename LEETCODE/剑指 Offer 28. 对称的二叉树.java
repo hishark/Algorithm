e83a6c0e7f90555d837877a04950fc2fdff80ada
777 <hishark@yeah.net>
2020-08-04 00:05:40 +0800

    Update 剑指 Offer 28. 对称的二叉树.java

--- LEETCODE/剑指 Offer 28. 对称的二叉树.java
+++ LEETCODE/剑指 Offer 28. 对称的二叉树.java
@@ -1,1 +1,25 @@
-null
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        else
+            return recursion(root.left, root.right);
+    }
+    public boolean recursion(TreeNode leftTree, TreeNode rightTree) {
+        if(leftTree == null && rightTree == null) 
+            return true;
+        if(leftTree == null || rightTree == null || leftTree.val != rightTree.val) 
+            return false;
+        return recursion(leftTree.left, rightTree.right) 
+                && recursion(leftTree.right, rightTree.left);
+    }
+}
