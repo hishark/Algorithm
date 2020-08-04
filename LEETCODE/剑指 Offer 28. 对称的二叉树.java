ee7b747dec4211b963cf1c87cf6de8e7676e6e01
777 <hishark@yeah.net>
2020-08-04 23:54:32 +0800

    Update 剑指 Offer 28. 对称的二叉树.java

--- LEETCODE/剑指 Offer 28. 对称的二叉树.java
+++ LEETCODE/剑指 Offer 28. 对称的二叉树.java
@@ -9,17 +9,22 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        // 空树显然对称
         if (root == null)
             return true;
-        else
-            return recursion(root.left, root.right);
+        // 非空树，检查左右子树是否对称
+        return recursion(root.left, root.right);
     }
-    public boolean recursion(TreeNode leftTree, TreeNode rightTree) {
-        if(leftTree == null && rightTree == null) 
+
+    // 检查L和R是否对称
+    public boolean recursion(TreeNode L, TreeNode R) {
+        // 空，对称
+        if (L == null && R == null)
             return true;
-        if(leftTree == null || rightTree == null || leftTree.val != rightTree.val) 
+        // 根不同，不对称
+        if (L == null || R == null || L.val != R.val)
             return false;
-        return recursion(leftTree.left, rightTree.right) 
-                && recursion(leftTree.right, rightTree.left);
+        // 若L的left和R的right相同，L的right和R的left相同，说明此树对称
+        return recursion(L.left, R.right) && recursion(L.right, R.left);
     }
 }
