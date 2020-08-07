8aaa4b5a37575560d1c16992597b9c063396b2b3
777 <hishark@yeah.net>
2020-08-07 23:37:52 +0800

    Update 100. 相同的树.java

--- LEETCODE/100. 相同的树.java
+++ LEETCODE/100. 相同的树.java
@@ -4,16 +4,21 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-//递归解决 dfs
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
-        if(p.val != q.val) return false;
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null || p.val != q.val)
+            return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        
     }
 }
