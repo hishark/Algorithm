3e4815ce581c1c653bcdb964afc04331ee153b17
777 <hishark@yeah.net>
2020-08-14 17:20:11 +0800

    Update 剑指 Offer 06. 从尾到头打印链表.java

--- LEETCODE/剑指 Offer 06. 从尾到头打印链表.java
+++ LEETCODE/剑指 Offer 06. 从尾到头打印链表.java
@@ -6,6 +6,8 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+// 简单粗暴无脑法，反转链表再输出
 class Solution {
     public int[] reversePrint(ListNode head) {
         int length = 0;
@@ -25,5 +27,50 @@ class Solution {
         }
 
         return res;
+    }
+}
+
+// 栈
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int len = 0;
+        while (head!=null) {
+             stack.push(head.val);
+             head = head.next;
+             len++;
+        }
+        int[] res = new int[len];
+        int i = 0;
+
+        while (!stack.empty()) {
+            res[i] = stack.pop();
+            i++;
+        }
+
+        return res;
+    }
+}
+
+// 递归
+class Solution {
+    ArrayList<Integer> tmp = new ArrayList<Integer>();
+
+    public int[] reversePrint(ListNode head) {
+        recursion(head);
+        int[] res = new int[tmp.size()];
+        for (int i=0;i<res.length;i++) {
+            res[i] = tmp.get(i);
+        }
+        return res;
+    }
+
+    // 递归的从尾到头添加结点值到tmp
+    // 每访问一个节点的时候，先递归添加它后面的结点，再添加它本身
+    public void recursion(ListNode head) {
+        if(head == null)
+            return;
+        recursion(head.next);
+        tmp.add(head.val);
     }
 }
