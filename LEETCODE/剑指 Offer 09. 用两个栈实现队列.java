c18d59fbe6eb5682c1f39be86f1e6c19b5544adf
777 <hishark@yeah.net>
2020-08-20 23:21:53 +0800

    Create 剑指 Offer 09. 用两个栈实现队列.java

---
+++ LEETCODE/剑指 Offer 09. 用两个栈实现队列.java
@@ -0,0 +1,38 @@
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
+class CQueue {
+    // 辅助栈
+    Stack<Integer> stackIn;
+    Stack<Integer> stackOut;
+
+    public CQueue() {
+        stackIn = new Stack<Integer>();
+        stackOut = new Stack<Integer>();
+    }
+    
+    public void appendTail(int value) {
+        stackIn.push(value);
+    }
+    
+    public int deleteHead() {
+        if(stackIn.empty() && stackOut.empty())
+            return -1;
+        
+        
+        // out栈不为空，就先pop
+        if(!stackOut.empty())
+            return stackOut.pop();
+        else{
+            // 先把in栈的全部pop到out栈中
+            while(!stackIn.empty()){
+                stackOut.push(stackIn.pop());
+            }
+            return stackOut.pop();
+        }
+    }
+}
+
