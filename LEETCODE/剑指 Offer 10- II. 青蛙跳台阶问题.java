4f326c90b021ed9adb724301dd0f8b569f138e1f
777 <hishark@yeah.net>
2020-08-21 15:44:48 +0800

    Create 剑指 Offer 10- II. 青蛙跳台阶问题.java

---
+++ LEETCODE/剑指 Offer 10- II. 青蛙跳台阶问题.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numWays(int n) {
+        // dp
+        // 状态数组
+        int[] dp = new int[n+1];
+        
+        // 边界值
+        if (n==0 || n==1) 
+            return 1;
+        
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // 状态转移方程
+        for (int i=2;i<=n;i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+}
