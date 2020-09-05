111f2b5a4e3e759af1d14b04d047ba95102ab90f
777 <hishark@yeah.net>
2020-09-05 23:58:31 +0800

    Update 剑指 Offer 16. 数值的整数次方.java

--- LEETCODE/剑指 Offer 16. 数值的整数次方.java
+++ LEETCODE/剑指 Offer 16. 数值的整数次方.java
@@ -17,3 +17,21 @@ class Solution {
         return preN < 0 ? 1/res : res;
     }
 }
+// ref: Krahets
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;
+        long b = n;
+        double res = 1.0;
+        if(b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        while(b > 0) {
+            if((b & 1) == 1) res *= x;
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+}
