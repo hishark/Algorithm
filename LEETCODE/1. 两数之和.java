4a454e9220800a225d331a7319a8ceaaf027e8f5
hishark <hishark@yeah.net>
2020-10-03 23:45:22 +0800

    update

--- LEETCODE/1. 两数之和.java
+++ LEETCODE/1. 两数之和.java
@@ -39,13 +39,12 @@ class OthersSolution{
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            int dvalue = target - nums[i];
-            if (map.containsKey(dvalue)){
-                return new int[]{map.get(dvalue), i};
-            }
+        for (int i=0;i<nums.length;i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) 
+                return new int[]{map.get(diff), i};
             map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No pairs of number");
+        return new int[]{-1, -1};
     }
 }
