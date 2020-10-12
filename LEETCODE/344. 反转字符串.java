8945250d92ce3fb4fa508f1c85eaa0ba4588560b
hishark <hishark@yeah.net>
2020-10-12 22:25:08 +0800

    update

--- LEETCODE/344. 反转字符串.java
+++ LEETCODE/344. 反转字符串.java
@@ -1,18 +1,12 @@
-//这个不太行
-class Solution {
-    public String reverseString(String s) {
-        StringBuffer test = new StringBuffer(s);
-        test.reverse();
-        s = new String(test);
-        return s;
-    }
-}
-
 //双指针
 class Solution {
     public void reverseString(char[] s) {
+        // 双指针，一个指向头
         int i = 0;
+        // 一个指向尾
         int j = s.length - 1;
+        // 两个指针碰面时才结束循环
+        // 「<」或者「<=」都可以，没差
         while(i<j) {
             char temp = s[i];
             s[i] = s[j];
---
+++ LEETCODE/剑指 Offer 31. 栈的压入、弹出序列.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        // 题目提示说「pushed 是 popped 的排列」，说明我们不需要考虑两个序列「长度不同」或者「元素不同」的情况
+        // 借助一个辅助栈模拟压入和弹出操作
+        Stack<Integer> stack = new Stack<>();
+
+        // 工具人i
+        int i = 0;
+
+        // 遍历入栈序列
+        for (int num: pushed) {
+            // 把入栈序列pushed的当前元素放入辅助栈stack
+            stack.push(num);
+            // 循环判断「stack栈顶元素 == popped序列的当前元素」是否成立
+            while(!stack.isEmpty() && stack.peek() == popped[i]) {
+                // 如果成立的话，就把该元素弹出辅助栈 stack，这样就完成了一次出入栈模拟操作
+                // 发射！
+                stack.pop();
+                // 每成功模拟一次出入栈操作，才能将 i 自增
+                i++;
+            }
+        }
+        // 如果最后辅助栈为空，说明模拟出入栈操作成功。
+        // 如果不为空，说明模拟失败。
+        return stack.isEmpty();
+    }
+}
