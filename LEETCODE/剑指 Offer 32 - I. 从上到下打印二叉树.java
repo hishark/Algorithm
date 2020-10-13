44bb3f605c9bed91d4c194b8ac52de5789383669
hishark <hishark@yeah.net>
2020-10-13 21:17:10 +0800

    update

---
+++ LEETCODE/剑指 Offer 32 - I. 从上到下打印二叉树.java
@@ -0,0 +1,47 @@
+// ref: Krahets
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] levelOrder(TreeNode root) {
+        // 判空
+        if (root == null)
+            return new int[0];
+
+        // BFS 需要使用队列
+        Queue<TreeNode> queue = new LinkedList<>();
+        // 先把根节点放入队列
+        queue.offer(root);
+
+        // 打印结果数组
+        ArrayList<Integer> res = new ArrayList<>();
+
+        // 队列为空时结束循环
+        while (!queue.isEmpty()) {
+            // 取出队头结点作为当前结点
+            TreeNode curNode = queue.poll();
+            // 先把当前结点放入结果数组
+            res.add(curNode.val);
+            // 然后开始判断当前结点的左右结点是否存在
+            // 如果存在的话，将其放入队列
+            if (curNode.left != null)
+                queue.add(curNode.left);
+            if (curNode.right != null )
+                queue.add(curNode.right);
+        }
+
+        // 将ArrayList转换为int[]即可
+        int[] finalRes = new int[res.size()];
+        for(int i=0;i<res.size();i++) 
+            finalRes[i] = res.get(i);
+
+        // 返回最终结果
+        return finalRes;
+    }
+}
--- LEETCODE/剑指 Offer 32 - II. 从上到下打印二叉树 II.java
+++ LEETCODE/剑指 Offer 32 - II. 从上到下打印二叉树 II.java
@@ -9,32 +9,48 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        // 判空
         if (root == null)
             return new ArrayList<>();
-        
+
+        // BFS 需要使用队列存储遍历结点
         Queue<TreeNode> queue = new LinkedList<>();
+        // 首先把根结点放入队列
         queue.offer(root);
+        // 最终的遍历结果
         List<List<Integer>> resList = new ArrayList<>();
+
+        // 队列为空时停止循环
         while (!queue.isEmpty()) {
+            // 记录下队列此时的长度，用于
             int curSize = queue.size();
+            // 工具人 i
             int i = 0;
+            // 当前层次的遍历结果
             List<Integer> curList = new ArrayList<>();
+            // 对此时队列中的所有结点依次进行判断
             while (i < curSize) {
+                // 取出队头结点作为当前结点
                 TreeNode curNode = queue.poll();
+                // 把当前结点加入当前层次的遍历结果数组
                 curList.add(curNode.val);
                 
+                // 判断当前结点的左右结点是否为空
+                // 如果不为空，就把节点加入到队列中，用于之后的遍历
                 if(curNode.left != null)
                     queue.offer(curNode.left);
-                
                 if(curNode.right != null)
                     queue.offer(curNode.right);
                 
+                // 工具人自增
                 i++;
             }
             
+            // 把当前层次的遍历结果放入最终结果中，继续下一层的遍历
             resList.add(curList);
         }
 
+        // 返回即可
         return resList;
     }
 }
