11699b5d4a8bcabd1c5519f6bd0e6f66e3bf855b
hishark <hishark@yeah.net>
2020-10-22 22:21:56 +0800

    update

--- LEETCODE/剑指 Offer 38. 字符串的排列.java
+++ LEETCODE/剑指 Offer 38. 字符串的排列.java
@@ -1,22 +1,60 @@
-import java.util.LinkedList;
-
+// ref: https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof/solution/mian-shi-ti-38-zi-fu-chuan-de-pai-lie-hui-su-fa-by/
 class Solution {
-
     // 全排列问题
     // 结果列表
     List<String> ans = new LinkedList<>();
 
     // 所有字符
-    char[] c;
+    char[] chars;
 
     public String[] permutation(String s) {
         // 把字符串转换为字符
-        c = s.toCharArray();
+        chars = s.toCharArray();
 
+        // 首先固定第 0 个字符
         dfs(0);
+
+        // 返回结果即可
+        // List.toArray(): https://blog.csdn.net/judyfun/article/details/50239127
+        return ans.toArray(new String[ans.size()]);
     }
 
-    public void dfs(int x) {
-        
+    // 深度优先搜索所有排列方案
+    public void dfs(int n) {
+        // 递归终止条件： 如果递归到了最后一个字符，就把目前的排列方案加入到结果列表里
+        if (n == chars.length - 1) {
+            ans.add(String.valueOf(chars));
+            return;
+        }
+
+        // 初始化一个 set 用于排除重复的字符
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = n; i < chars.length; i++) {
+            // set 中如果存在当前字符，说明重复，因而剪枝，继续查看下一个字符
+            if (set.contains(chars[i]))
+                continue;
+
+            // 把当前字符加入到 set 中
+            set.add(chars[i]);
+
+            // 交换 chars[i] 和 chars[x]，将 chars[i] 固定在第 n 位
+            swap(i, n);
+
+            // 固定第 n+1 位字符
+            dfs(n + 1);
+
+            // 恢复交换，继续查看下一个字符
+            swap(i, n);
+        }
+
     }
+
+    // 交换 chars 中位置 x 和 y 上的字符
+    public void swap(int x, int y) {
+        char tmp = chars[x];
+        chars[x] = chars[y];
+        chars[y] = tmp;
+    }
+
 }
---
+++ LEETCODE/剑指 Offer 40. 最小的k个数.java
@@ -0,0 +1,62 @@
+// 排序
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        Arrays.sort(arr);
+        int[] res = new int[k];
+        for (int i=0;i<k;i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+}
+
+
+// 堆
+// ref: https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/solution/zui-xiao-de-kge-shu-by-leetcode-solution/
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        // 数组中最小的 k 个数
+        int[] ans = new int[k];
+
+        // 判空
+        if (k == 0)
+            return ans;
+
+        /**
+         *  Java中默认的为小根堆，实现大根堆需要重写一下 Comparator
+         *  题目要求前 k 小的数字，所以需要使用一个容量为 k 的大根堆
+         * 
+         *  遍历数组的时候，若堆的大小小于 k，就直接把数字放进去
+         *  若堆的大小已经为 k，需要判断当前数字和堆顶数字的大小：
+         *      1. 若当前数字 >= 堆顶数字，那么这个数就直接跳过
+         *      2. 若当前数字 < 堆顶数字，那么就先把堆顶数字 poll 掉，然后把当前数字放入堆中
+         * 
+         */
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+
+        // 首先，先把数字的前 k 个数字全部怼入堆中
+        for (int i=0;i<k;i++) {
+            maxHeap.offer(arr[i]);
+        }
+
+        // 接着继续遍历数组中剩下的数字
+        for (int i=k;i<arr.length;i++) {
+            // 如果「当前遍历的数字」 <「堆顶数字」
+            if (maxHeap.peek() > arr[i]) {
+                // 就把堆顶数字送出去
+                maxHeap.poll();
+                // 并把当前数字放入堆中
+                maxHeap.offer(arr[i]);
+            }
+            // 如果「当前遍历的数字」 >=「堆顶数字」，直接跳过即可
+        }
+
+        // 此时，大根堆中已经存好了前 k 小的数字们，放入结果数组即可
+        for(int i=0;i<k;i++) 
+            ans[i] = maxHeap.poll();
+    } 
+}
