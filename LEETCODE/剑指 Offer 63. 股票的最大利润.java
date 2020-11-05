ad76b63ec95a00edb0c79f9f895f700bfc3fe7d5
hishark <hishark@yeah.net>
2020-11-05 23:53:51 +0800

    update

---
+++ LEETCODE/剑指 Offer 63. 股票的最大利润.java
@@ -0,0 +1,11 @@
+// ref: https://leetcode-cn.com/problems/gu-piao-de-zui-da-li-run-lcof/solution/mian-shi-ti-63-gu-piao-de-zui-da-li-run-dong-tai-2/
+class Solution {
+    public int maxProfit(int[] prices) {
+        // 状态数组
+        // dp[i] 表示以 prices[i] 结尾的子数组的最大利润（也就是前i天的最大利润）
+        int[] dp = new int[prices.length];
+
+        // 状态转移方程
+        
+    }
+}
