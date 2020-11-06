c06217b32b4d683acce5fbb9bc837ae2e95d975b
hishark <hishark@yeah.net>
2020-11-06 19:48:30 +0800

    update

---
+++ LEETCODE/剑指 Offer 41. 数据流中的中位数.java
@@ -0,0 +1,47 @@
+// ref: https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/you-xian-dui-lie-wu-fei-hua-jian-dan-yi-dong-by-je/
+// https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/mian-shi-ti-41-shu-ju-liu-zhong-de-zhong-wei-shu-y/
+class MedianFinder {
+    // 使用一个小顶堆和一个大顶堆来分别保存较大的一半和较小的一半
+    PriorityQueue<Integer> minHeap, maxHeap;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        // PriorityQueue 默认为小顶堆
+        // minHeap 为小顶堆。用于保存较大的一半数字
+        minHeap = new PriorityQueue<>();
+        // maxHeap 为大顶堆，需要重写 Comparator。用于保存较小的一半数字
+        maxHeap = new PriorityQueue<>((x, y) -> (y - x));
+        // 重写 Comparator 完整的写法：
+        // maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
+        //     public int compare(Integer x, Integer y) {
+        //         return y - x;
+        //     }
+        // });
+    }
+    
+    public void addNum(int num) {
+        // 需要保持两个堆的平衡，最大堆的大小要始终等于最小堆的大小，或者等于最小堆的大小 + 1
+        // 首先向最大堆中插入元素
+        maxHeap.offer(num);
+        // 最大堆中插入完，就从最大堆中取出最大的元素，插入最小堆中
+        minHeap.offer(maxHeap.poll());
+        // 此时如果两个堆不平衡，就把最小堆的元素放到最大堆里去
+        if (minHeap.size() > maxHeap.size()) 
+            maxHeap.offer(minHeap.poll());
+    }
+    
+    public double findMedian() {
+        // 如果两个堆的大小相等，就取出两个堆的堆顶元素求平均值
+        if (maxHeap.size() == minHeap.size()) 
+            return (maxHeap.poll() + minHeap.poll()) * 0.5;
+        // 若两个堆的大小不相等，直接取出最大堆的堆顶元素即可
+        return maxHeap.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
--- LEETCODE/剑指 Offer 63. 股票的最大利润.java
+++ LEETCODE/剑指 Offer 63. 股票的最大利润.java
@@ -1,11 +1,50 @@
 // ref: https://leetcode-cn.com/problems/gu-piao-de-zui-da-li-run-lcof/solution/mian-shi-ti-63-gu-piao-de-zui-da-li-run-dong-tai-2/
 class Solution {
     public int maxProfit(int[] prices) {
+        // 判空
+        if (prices.length == 0)
+            return 0;
+
         // 状态数组
         // dp[i] 表示以 prices[i] 结尾的子数组的最大利润（也就是前i天的最大利润）
         int[] dp = new int[prices.length];
 
-        // 状态转移方程
-        
+        // 前几日的最低价格
+        int minPrice = Integer.MAX_VALUE;
+        // 股票的最大利润
+        int maxProfit = 0;
+
+        // 边界值
+        dp[0] = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            // 遍历的同时更新股票的最低价格
+            minPrice = Math.min(minPrice, prices[i]);
+            // 当前最大利润为【前一天的最大利润】和【当日股价 - 前几日中的最低股价】中的较大者
+            dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
+            // 更新最大利润
+            maxProfit = Math.max(dp[i], maxProfit);
+        }
+
+        return maxProfit;
+    }
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        // 前几日的股票最低价格
+        int minPrice = Integer.MAX_VALUE;
+        // 股票的最大利润
+        int maxProfit = 0;
+
+        // 遍历股价
+        for (int price : prices) {
+            // 更新股票最低价格
+            minPrice = Math.min(minPrice, price);
+            // 更新最大利润，当前最大利润为【前一天的最大利润】和【当日股价 - 前几日中的最低股价】中的较大者
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
     }
 }
