95412abb242d9c462ec142501f0588475177d630
hishark <hishark@yeah.net>
2021-02-02 23:59:53 +0800

    update

---
+++ LEETCODE/888. 公平的糖果交换.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int sumA = Arrays.stream(A).sum();
+        int sumB = Arrays.stream(B).sum();
+        int delta = (sumA - sumB) / 2;
+        Set<Integer> rec = new HashSet<Integer>();
+        for (int num : A) {
+            rec.add(num);
+        }
+        int[] ans = new int[2];
+        for (int y : B) {
+            int x = y + delta;
+            if (rec.contains(x)) {
+                ans[0] = x;
+                ans[1] = y;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+// ref：https://leetcode-cn.com/problems/fair-candy-swap/solution/gong-ping-de-tang-guo-jiao-huan-by-leetc-tlam/
+
