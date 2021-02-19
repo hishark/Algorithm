4bc46301c6a8d1a975698bfd850e3f2a25184524
hishark <hishark@yeah.net>
2021-02-19 23:21:17 +0800

    update

--- LEETCODE/141. 环形链表.java
+++ LEETCODE/141. 环形链表.java
@@ -1,1 +1,30 @@
-#todo
+/**
+ * Definition for singly-linked list. 
+ * class ListNode { 
+ * int val; 
+ * ListNode next;
+ * ListNode(int x) 
+ * { 
+ * val = x; next = null; 
+ * } 
+ * }
+ * 使用双指针，一个指针每次移动一个节点，一个指针每次移动两个节点，如果存在环，那么这两个指针一定会相遇。
+ * 运动是相对的，快指针相对于慢指针，就相当于慢指针静止，快指针每次移动一步。这样只要是在环中，快指针肯定会与慢指针相遇。
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode l1 = head, l2 = head.next;
+        while (l1 != null && l2 != null && l2.next != null) { // 因为l2一次移动两步所以也要判断一下l2.next
+            // 两个指针相遇了就是有环啦！
+            if (l1 == l2) {
+                return true;
+            }
+            l1 = l1.next;
+            l2 = l2.next.next;
+        }
+        return false;
+    }
+}
---
+++ LEETCODE/142. 环形链表 II.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = head;
+        Set<ListNode> visited = new HashSet<ListNode>();
+        while (pos != null) {
+            if (visited.contains(pos)) {
+                return pos;
+            } else {
+                visited.add(pos);
+            }
+            pos = pos.next;
+        }
+        return null;
+    }
+}
